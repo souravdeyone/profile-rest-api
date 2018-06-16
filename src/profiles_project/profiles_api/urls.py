079cecd0e37d6d252dc8a01543472377cc97f4b1
Sourav <souravdeyone@gmail.com>
2018-06-16 15:39:17 -0700

    Added improved permissions to user profile feed items API

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -8,6 +8,7 @@ router = DefaultRouter()
 router.register('Hello-viewset', views.HelloViewSets, base_name= 'hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet, base_name = 'login')
+router.register('feed', views.UserProfileFeedViewset)
 
 
 urlpatterns = [
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,7 +8,8 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import filters
 from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import models
 from . import permissions
@@ -141,3 +142,17 @@ class MyProfileApiView(APIView):
 
         current_user = self.serializer_class(request.user)
         return Response(current_user.data)
+
+
+class UserProfileFeedViewset(viewsets.ModelViewSet):
+    """Handles creating, radings and updating profile feed items"""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
