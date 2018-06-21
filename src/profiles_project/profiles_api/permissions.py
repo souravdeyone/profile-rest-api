717a1dad86d4d347672d59e39da0402644572100
Sourav <souravdeyone@gmail.com>
2018-06-21 00:50:40 -0700

    reverted permissions.py

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -10,7 +10,7 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.id != request.user.id
+        return obj.id == request.user.id
 
 class PostOwnStatus(permissions.BasePermission):
     """Allow users to update their own status"""
